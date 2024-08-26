c25487c3d0960d06db11e9627d28bcd905008cdf
Maxime Kaberuka <maximekaberuka2@gmail.com>
2024-08-26 14:05:51 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Bienvenue sur Kasa !</h1>
+      <p>Ceci est ma première application React.</p>
+    </div>
+  );
+}
+
+export default App;
